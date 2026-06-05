7a92051c08e4505ac889d58c12326729aee9d923
shamilahmdt <shamilahmd12@gmail.com>
2026-06-05 17:28:56 +0530

    updates

--- ecommerce_backend/config/firebase.js
+++ ecommerce_backend/config/firebase.js
@@ -1,5 +1,7 @@
 const admin = require("firebase-admin");
 const dotenv = require("dotenv");
+const fs = require("fs");
+const path = require("path");
 
 dotenv.config();
 
@@ -8,7 +10,16 @@ try {
   if (process.env.FIREBASE_SERVICE_ACCOUNT) {
     serviceAccount = JSON.parse(process.env.FIREBASE_SERVICE_ACCOUNT);
   } else {
-    serviceAccount = require("../serviceAccountKey.json");
+    const localPath = path.join(__dirname, "../serviceAccountKey.json");
+    const rootPath = path.join(__dirname, "../../serviceAccountKey.json");
+
+    if (fs.existsSync(localPath)) {
+      serviceAccount = require(localPath);
+    } else if (fs.existsSync(rootPath)) {
+      serviceAccount = require(rootPath);
+    } else {
+      throw new Error(`Cannot find serviceAccountKey.json at either '${localPath}' or '${rootPath}'`);
+    }
   }
   admin.initializeApp({
     credential: admin.credential.cert(serviceAccount),
